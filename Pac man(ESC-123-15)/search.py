32ddaf87b0c5d01beb39043a4594eb029cbdb717
EMMANUEL-MANGULENJE <mangulenjemmanuel@gmail.com>
2020-12-04 09:01:42 -0800

    MY ASSIGNMENT

---
+++ Pac man(ESC-123-15)/search.py
@@ -0,0 +1,146 @@
+# search.py
+# ---------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+In search.py, you will implement generic search algorithms which are called by
+Pacman agents (in searchAgents.py).
+"""
+
+import util
+
+class SearchProblem:
+    """
+    This class outlines the structure of a search problem, but doesn't implement
+    any of the methods (in object-oriented terminology: an abstract class).
+
+    You do not need to change anything in this class, ever.
+    """
+
+    def getStartState(self):
+        """
+        Returns the start state for the search problem.
+        """
+        util.raiseNotDefined()
+
+    def isGoalState(self, state):
+        """
+          state: Search state
+
+        Returns True if and only if the state is a valid goal state.
+        """
+        util.raiseNotDefined()
+
+    def getSuccessors(self, state):
+        """
+          state: Search state
+
+        For a given state, this should return a list of triples, (successor,
+        action, stepCost), where 'successor' is a successor to the current
+        state, 'action' is the action required to get there, and 'stepCost' is
+        the incremental cost of expanding to that successor.
+        """
+        util.raiseNotDefined()
+
+    def getCostOfActions(self, actions):
+        """
+         actions: A list of actions to take
+
+        This method returns the total cost of a particular sequence of actions.
+        The sequence must be composed of legal moves.
+        """
+        util.raiseNotDefined()
+
+
+def tinyMazeSearch(problem):
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    return  [s, s, w, s, w, w, s, w]
+
+def depthFirstSearch(problem):
+    fringe = util.Stack()
+    visited = []
+    actionList = []
+    fringe.push((problem.getStartState(), actionList))
+    while fringe:
+        node, actions = fringe.pop()
+        if not node in visited:
+            visited.append(node)
+            if problem.isGoalState(node):
+                return actions
+            for successor in problem.getSuccessors(node):
+                coordinate, direction, cost = successor
+                nextActions = actions + [direction]
+                fringe.push((coordinate, nextActions))
+    return []
+
+#breadth first search problem function
+def breadthFirstSearch(problem): 
+    fringe = util.Queue()
+    visited = []
+    actionList = []
+    fringe.push((problem.getStartState(), actionList))
+    while fringe:
+        node, actions = fringe.pop()
+        if not node in visited:
+            visited.append(node)
+            if problem.isGoalState(node):
+                return actions
+            for successor in problem.getSuccessors(node):
+                coordinate, direction, cost = successor
+                nextActions = actions + [direction]
+                fringe.push((coordinate, nextActions))
+    return []
+
+#uniform search first search problem function
+def uniformCostSearch(problem):
+    fringe = util.PriorityQueue()
+    visited = []
+    actionList = []
+    fringe.push((problem.getStartState(), actionList), problem)
+    while fringe:
+        node, actions = fringe.pop()
+        if not node in visited:
+            visited.append(node)
+            if problem.isGoalState(node):
+                return actions
+            for successor in problem.getSuccessors(node):
+                coordinate, direction, cost = successor
+                nextActions = actions + [direction]
+                nextCost = problem.getCostOfActions(nextActions)
+                fringe.push((coordinate, nextActions), nextCost)
+    return []
+
+def nullHeuristic(state, problem=None):
+    return 1
+
+#aStarSearch search problem function
+def aStarSearch(problem, heuristic=nullHeuristic):
+    fringe = util.PriorityQueue()
+    visited = []   
+    actionList = []
+    fringe.push((problem.getStartState(), actionList), heuristic(problem.getStartState(), problem))
+    while fringe:
+        node, actions = fringe.pop()
+        if not node in visited:
+            visited.append(node)
+            if problem.isGoalState(node):
+                return actions
+            for successor in problem.getSuccessors(node):
+                coordinate, direction, cost = successor
+                nextActions = actions + [direction]
+                nextCost = problem.getCostOfActions(nextActions) + \
+                               heuristic(coordinate, problem)
+                fringe.push((coordinate, nextActions), nextCost)
+    return []
